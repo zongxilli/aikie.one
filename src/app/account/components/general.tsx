59cc85961e2095da844d35234ae8bee79502ba7b
zongxi2014 <zongxi2014@gmail.com>
2024-06-28 19:27:18 -0400

    refactor

---
+++ src/app/account/components/general.tsx
@@ -0,0 +1,187 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { useToast } from '@/components/ui/use-toast';
+import { updateUserImage, updateUserProfile } from '@/lib/userActions';
+import { useUserStore } from '@/providers/user';
+
+import { UserProfile } from '../../types/users';
+
+type UserField = Omit<UserProfile, 'id' | 'image'>;
+
+const General = () => {
+	const { toast } = useToast();
+	const { user } = useUserStore((state) => state);
+
+	const [userField, setUserField] = useState<UserField>({
+		name: user?.name ?? '',
+		email: user?.email ?? '',
+	});
+	const [imageFile, setImageFile] = useState<File | null>(null);
+	const [imageFilePreviewUrl, setImageFilePreviewUrl] = useState<
+		string | undefined
+	>(undefined);
+
+	useEffect(() => {
+		setUserField({
+			name: user?.name ?? '',
+			email: user?.email ?? '',
+		});
+	}, [user]);
+
+	useEffect(() => {
+		if (imageFile) {
+			const objectUrl = URL.createObjectURL(imageFile);
+			setImageFilePreviewUrl(objectUrl);
+
+			return () => URL.revokeObjectURL(objectUrl);
+		}
+	}, [imageFile]);
+
+	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (event.target.files && event.target.files[0]) {
+			const file = event.target.files[0];
+			setImageFile(file);
+		}
+	};
+
+	const handleUpdateUserProfile = async () => {
+		try {
+			if (user?.id) {
+				await updateUserProfile(
+					user?.id,
+					userField.name,
+					userField.email
+				);
+
+				toast({
+					variant: 'success',
+					title: 'Update successful',
+					description: 'User profile has been updated',
+				});
+			}
+		} catch (error) {
+			toast({
+				variant: 'destructive',
+				title: 'Update failed',
+				description: 'Please try again later',
+			});
+		}
+	};
+
+	const handleUpdateUserImage = async () => {
+		try {
+			if (user?.id && imageFile) {
+				await updateUserImage(user.id, imageFile);
+
+				toast({
+					variant: 'success',
+					title: 'Update successful',
+					description: 'User image has been updated',
+				});
+			}
+		} catch (error) {
+			toast({
+				variant: 'destructive',
+				title: 'Upload failed',
+				description: 'Please try again later',
+			});
+		}
+	};
+
+	const onGeneralFormChange = (field: keyof UserField, value: string) => {
+		setUserField((prevForm) => {
+			const newForm = { ...prevForm };
+			newForm[field] = value;
+			return newForm;
+		});
+	};
+
+	return (
+		<div className='flex-grow flex flex-col gap-4'>
+			<Card>
+				<CardHeader className='flex flex-row items-start justify-between'>
+					<div className='flex flex-col items-start gap-2'>
+						<CardTitle>Avatar</CardTitle>
+						<CardDescription>
+							Your avatar as displayed in the application.
+						</CardDescription>
+					</div>
+					<Avatar className='h-12 w-12'>
+						<AvatarImage src={imageFilePreviewUrl ?? user?.image} />
+						<AvatarFallback>IMG</AvatarFallback>
+					</Avatar>
+				</CardHeader>
+				<CardContent>
+					<Input type='file' onChange={handleFileChange} />
+				</CardContent>
+
+				<CardFooter className='border-t px-6 py-4'>
+					<Button
+						disabled={imageFile === null}
+						onClick={handleUpdateUserImage}
+					>
+						Save
+					</Button>
+				</CardFooter>
+			</Card>
+
+			<Card>
+				<CardHeader>
+					<CardTitle>Name</CardTitle>
+					<CardDescription>
+						Your user name used to identify you in this app.
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Input
+						type='text'
+						value={userField.name}
+						onChange={(e) => {
+							onGeneralFormChange('name', e.target.value);
+						}}
+					/>
+				</CardContent>
+				<CardHeader>
+					<CardTitle>Email</CardTitle>
+					<CardDescription>
+						You are not allowed to change your email address.
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Input
+						type='email'
+						value={userField.email}
+						onChange={(e) => {
+							onGeneralFormChange('email', e.target.value);
+						}}
+						disabled
+					/>
+				</CardContent>
+
+				<CardFooter className='border-t px-6 py-4'>
+					<Button
+						disabled={userField.name === user?.name}
+						onClick={handleUpdateUserProfile}
+					>
+						Save
+					</Button>
+				</CardFooter>
+			</Card>
+		</div>
+	);
+};
+
+export default General;
--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,146 +1,51 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { Button } from '@/components/ui/button';
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { useToast } from '@/components/ui/use-toast';
-import { updateUserProfile } from '@/lib/userActions';
-import { useUserStore } from '@/providers/user';
 
-import { UserProfile } from '../types/users';
-
-type UserField = Omit<UserProfile, 'id'>;
+import General from './components/general';
 
 export default function AccountPage() {
-	const { toast } = useToast();
-
-	const { user } = useUserStore((state) => state);
-
 	const [tab, setTab] = useState(0);
 
-	const [userField, setUserField] = useState<UserField>({
-		name: user?.name ?? '',
-		email: user?.email ?? '',
-		image: user?.image ?? '',
-	});
-
-	const handleUpdateUserProfile = async () => {
-		try {
-			if (user?.id) {
-				await updateUserProfile(
-					user?.id,
-					userField.name,
-					userField.email,
-					userField.image
-				);
-
-				toast({
-					variant: 'success',
-					title: 'Update successful',
-					description: 'User profile has been updated',
-				});
-			}
-		} catch (error) {
-			toast({
-				variant: 'destructive',
-				title: 'Update failed',
-				description: 'Please try again later',
-			});
-		}
+	const renderSideBar = () => {
+		return (
+			<div className='w-20 md:w-40 flex flex-col text-sm text-muted-foreground'>
+				<Button
+					className='flex justify-start'
+					variant='ghost'
+					onClick={() => setTab(0)}
+				>
+					General
+				</Button>
+				<Button
+					className='flex justify-start'
+					variant='ghost'
+					onClick={() => setTab(1)}
+				>
+					Danger
+				</Button>
+			</div>
+		);
 	};
 
-	useEffect(() => {
-		setUserField({
-			name: user?.name ?? '',
-			email: user?.email ?? '',
-			image: user?.image ?? '',
-		});
-	}, [user]);
+	const renderContent = () => {
+		switch (tab) {
+			case 0:
+				return <General />;
 
-	const onGeneralFormChange = (field: keyof UserField, value: string) => {
-		setUserField((prevForm) => {
-			const newForm = { ...prevForm };
-			newForm[field] = value;
-			return newForm;
-		});
+			case 1:
+				return <div>placeholder</div>;
+		}
 	};
 
 	return (
 		<main className='container h-full flex flex-col gap-4 p-4 md:gap-8 md:p-10'>
-			<div className=''>
-				<h1 className='text-3xl font-semibold'>Settings</h1>
-				<h1 className='text-3xl font-semibold'>{user?.name}</h1>
-			</div>
+			<h1 className='text-3xl font-semibold'>Settings</h1>
 			<div className='w-full flex items-start justify-between gap-2 md:gap-6'>
-				<div className='w-20 md:w-40 flex flex-col text-sm text-muted-foreground'>
-					<Button
-						className='flex justify-start'
-						variant='ghost'
-						onClick={() => setTab(0)}
-					>
-						General
-					</Button>
-					<Button
-						className='flex justify-start'
-						variant='ghost'
-						onClick={() => setTab(1)}
-					>
-						Danger
-					</Button>
-				</div>
-				<div className='flex-grow flex flex-col gap-4'>
-					<Card>
-						<CardHeader>
-							<CardTitle>User Info</CardTitle>
-							<CardDescription>
-								Your user name used to identify you in this app.
-							</CardDescription>
-						</CardHeader>
-						<CardContent>
-							<Input
-								type='text'
-								value={userField.name}
-								onChange={(e) => {
-									onGeneralFormChange('name', e.target.value);
-								}}
-							/>
-						</CardContent>
-						<CardHeader>
-							<CardTitle>Email</CardTitle>
-							<CardDescription>
-								You are not allowed to change your email address
-							</CardDescription>
-						</CardHeader>
-						<CardContent>
-							<Input
-								type='email'
-								value={userField.email}
-								onChange={(e) => {
-									onGeneralFormChange(
-										'email',
-										e.target.value
-									);
-								}}
-								disabled
-							/>
-						</CardContent>
-
-						<CardFooter className='border-t px-6 py-4'>
-							<Button onClick={handleUpdateUserProfile}>
-								Save
-							</Button>
-						</CardFooter>
-					</Card>
-				</div>
+				{renderSideBar()}
+				{renderContent()}
 			</div>
 		</main>
 	);
