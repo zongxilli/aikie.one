7c935c29cfe2c6573b47e52d1fe3a26feac0cd69
zongxi2014 <zongxi2014@gmail.com>
2024-06-28 17:03:23 -0400

    remove useUserProfile custom hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,12 +2,5 @@ import useOnMountEffect from './useOnMountEffect';
 import usePrevious from './usePrevious';
 import useSupabase from './useSupabase';
 import useThemeToggle from './useThemeToggle';
-import useUserProfile from './useUserProfile';
 
-export {
-	useOnMountEffect,
-	usePrevious,
-	useSupabase,
-	useThemeToggle,
-	useUserProfile,
-};
+export { useOnMountEffect, usePrevious, useSupabase, useThemeToggle };
